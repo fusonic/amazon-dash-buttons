4bb7b5844e4e40229560ac36f1cbb45f386a37a6
René Zumtobel <renezumtobel@gmail.com>
2018-07-12 11:18:50 +0200

    Added Slack support (#6)

--- Dockerfile
+++ Dockerfile
@@ -4,12 +4,13 @@ MAINTAINER Fusonic "office@fusonic.net"
 
 ARG DEBIAN_FRONTEND=noninteractive
 
-RUN apt-get update \
-    && apt-get install -yyq libpcap-dev git npm python make nodejs-legacy \
+RUN apt-get update && apt-get install curl -yyq && curl -sL https://deb.nodesource.com/setup_8.x | bash \
+    && apt-get update \
+    && apt-get install -yyq libpcap-dev git python make nodejs build-essential \
     && rm -rf /tmp/* /var/tmp/* /var/lib/apt/lists/* /usr/share/man
 
 COPY files /
 WORKDIR /opt/dash-button
-RUN npm install
+RUN node -v && npm -v && npm install
 
 CMD /boot
---
+++ files/opt/dash-button/scripts/providers/slack.js
@@ -0,0 +1,31 @@
+// require modules needed to send Slack messages
+const { WebClient } = require('@slack/client');
+
+// export module for main script
+module.exports = {
+    dashHandler : function (sender, providerConfig) {
+        postMessage(sender, providerConfig);
+    }
+};
+
+// postMessage uses the Slack api's method chat.postMessage
+// Documentation:
+// https://api.slack.com/methods/chat.postMessage
+function postMessage(sender, providerConfig) {
+    const web = new WebClient(providerConfig.token);
+    let arguments = {};
+
+    for (let [key, value] of Object.entries(providerConfig)) {
+        arguments[key] = value;
+    }
+
+    for (let [key, value] of Object.entries(sender.providerConfig)) {
+        arguments[key] = value;
+    }
+
+    web.chat.postMessage(arguments).then((res) => {
+        console.log('Slack: Sent message: ', res.message.text);
+    }).catch(error => {
+        console.error('Slack: an error occurred: ', error.data.error);
+    });
+}
